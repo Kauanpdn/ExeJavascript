75f76acee1aaff97e68bf3bca8270d7f1daa527f
OkauanDev <teko4278@gmail.com>
2025-06-09 20:57:21 -0300

    finalizando exercício

--- exe21.js
+++ exe21.js
@@ -4,5 +4,29 @@
 
 
 function estaçõesAno(n){
-    
+    if(n >= 1 && n<=3){
+        console.log("Verão")
+    }
+    if(n >= 4 && n <= 6){
+        console.log("Primavera")
+    }
+    if(n >= 7 && n <= 9){
+        console.log("Inverno")
+    }
+    if(n >= 10 && n <= 12){
+        console.log("Outono")
+    }
 }
+
+estaçõesAno(1)
+estaçõesAno(3)
+estaçõesAno(4)
+estaçõesAno(6)
+estaçõesAno(7)
+estaçõesAno(9)
+estaçõesAno(10)
+estaçõesAno(12)
+estaçõesAno(2)
+estaçõesAno(5)
+estaçõesAno(8)
+estaçõesAno(11)
---
+++ exe22.js
@@ -0,0 +1,3 @@
+// Exercicio 22 
+// escreva um programa que determina o número
+// de dia em um mês 
